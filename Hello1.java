15c3204bd0100d6f2924eb3aeca03e663dc132de
Rutuja Kumbhar <rutujakumbhar98@gmail.com>
2021-02-24 14:52:34 +0530

    Added function

---
+++ Hello1.java
@@ -0,0 +1,24 @@
+class Demo {
+    public static void fibonacci()
+    {
+    int i,fact=1;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter no");
+    int no=sc.nextInt();
+    for(i=1; i<=no; i++)
+    {
+    fact=fact*i;
+    }
+    System.out.println(fact);
+    }
+}
+class Hello1
+{
+    
+    public static calFactorial(x){}
+    public static void main(String args[]){
+        System.out.println("Hello, World!");
+    }
+
+
+}
